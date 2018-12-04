c887df28b3cbd7afc85b060c52f4a988fcc23bc1
stevenaeola <s.p.bradleydurham.ac.uk>
2018-12-04 12:08:28 +0000

    control ball from DOM

---
+++ ball/ball.js
@@ -0,0 +1,20 @@
+class Ball{
+    constructor(x, y, r, colour){
+	this.x = x || 50;
+	this.y = y || 50;
+	this.radius = r || 20;;
+	this.colour = colour || "red";
+    }
+
+    setColour(colour){
+	this.colour = colour;
+    }
+    
+    draw(){
+	fill(this.colour);
+	ellipse(this.x, this.y,
+		this.radius*2, this.radius*2);
+    }
+}
+
+
