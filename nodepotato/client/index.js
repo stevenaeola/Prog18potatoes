143e1b25913e2ca1f06575b7c0260b26d0445281
dcs0spb <s.p.bradley@durham.ac.uk>
2019-03-10 22:04:25 +0000

    submit new potata via POST fetch closes #3

--- nodepotato/client/index.js
+++ nodepotato/client/index.js
@@ -1,4 +1,4 @@
-window.addEventListener('click', async function(event){
+document.getElementById('get').addEventListener('click', async function(event){
   let response = await fetch('http://127.0.01:8090/list');
   let body = await response.text();
   document.getElementById('content').innerHTML = "<ul>";
@@ -11,4 +11,25 @@ window.addEventListener('click', async function(event){
   document.getElementById('content').innerHTML += "</ul>";
 
 
+});
+
+document.getElementById('add_potato').addEventListener('submit', async function(event){
+  event.preventDefault();
+  
+  try{
+    let potato_type = document.getElementById('potato_type').value;
+    let response = await fetch('http://127.0.01:8090/add',
+                               {
+                                 method: "POST",
+                                 headers: {
+                                   "Content-Type": "application/x-www-form-urlencoded"
+                                 },
+                                 body: "potato_type=" + potato_type
+                               });
+    if(!response.ok){
+      throw new Error("problem adding potato" + response.code);
+    }
+  } catch (error) {
+    alert ("problem: " + error);
+  }
 });
